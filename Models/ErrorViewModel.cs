b205301cfc63cf0fdf9bcf1f5aa9ffb1d5e37a14
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2026-07-14 19:47:39 +0530

    Initial commit - NASA APOD Gallery

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NasaApodGallery.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
